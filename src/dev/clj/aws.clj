cc91d58ea851830b2c759ee9c8d867a76ea6a2ae
Stanislas Nanchen <stan@hood.ch>
2017-10-19 16:08:31 +0200

    fix and reorganzie aws.clj

--- src/dev/clj/aws.clj
+++ src/dev/clj/aws.clj
@@ -113,6 +113,7 @@
                    :registry-id aws-account-id
                    :repository-names [repository-name])))
 
+
 ;; # Utilities
 
 (defn encode-base64 [^String text]
@@ -124,14 +125,6 @@
 
 ;; # ECS Cluster
 
-
-(defn role-arn [role-name]
-  (try
-    (sp/select-one! [:role :arn]
-                    (iam/get-role
-                      :role-name role-name))
-    (catch NoSuchEntityException _)))
-
 (defn create-ecs-cluster []
   (ecs/create-cluster
     :cluster-name ecs-cluster-name))
@@ -139,16 +132,67 @@
 (defn ecs-cluster-arn []
   (str "arn:aws:ecs:eu-west-1:" aws-account-id ":cluster/" ecs-cluster-name))
 
-(defn create-cloudwatch-log-group []
-  (cwl/create-log-group
-    :log-group-name cloudwatch-log-group-name))
 
-(defn cloudwatch-log-group-arn []
-  (sp/select-one [:log-groups sp/ALL :arn]
-                 (cwl/describe-log-groups
-                   :log-group-name-prefix cloudwatch-log-group-name)))
+;; # IP and DNS
 
-(defn create-ec2-instance-role [env]
+(defn create-eip []
+  (ec2/allocate-address
+    :domain DomainType/Vpc))
+
+(defn dns-zone-id []
+  (let [zone-name (str dns-zone-name ".")]
+    (sp/select-one!
+      [:hosted-zones sp/ALL #(= zone-name (:name %)) :id]
+      (r53/list-hosted-zones))))
+
+(defn create-dns-record [ip]
+  (r53/change-resource-record-sets
+    :hosted-zone-id (dns-zone-id)
+    :change-batch {:changes [{:action              ChangeAction/CREATE
+                              :resource-record-set (merge
+                                                     (if ip
+                                                       {:resource-records [{:value ip}]
+                                                        :ttl              300})
+                                                     {:name dns-name
+                                                      :type RRType/A})}]}))
+
+;; # EC2 instance
+
+(defn vpc-id []
+  (sp/select-one!
+    [:vpcs sp/ALL :vpc-id]
+    (ec2/describe-vpcs
+      :filters [{:name   "isDefault"
+                 :values [true]}])))
+
+(defn authorize-world-ingress [group-id port]
+  (ec2/authorize-security-group-ingress
+    :group-id group-id
+    :ip-protocol "tcp"
+    :from-port port
+    :to-port port
+    :cidr-ip "0.0.0.0/0"))
+
+(defn create-ec2-instance-security-group []
+  (let [sg-name  ec2-instance-security-group-name
+        group-id (:group-id
+                   (ec2/create-security-group
+                     :group-name sg-name
+                     :description (str project-name " HTTP/S")
+                     :vpc-id (vpc-id)))]
+    (authorize-world-ingress group-id 80)
+    (authorize-world-ingress group-id 443)
+    group-id))
+
+(defn ec2-instance-security-group-id []
+  (sp/select-one! [:security-groups sp/ALL :group-id]
+                  (ec2/describe-security-groups
+                    :filters [{:name   "group-name"
+                               :values [ec2-instance-security-group-name]}
+                              {:name   "vpc-id"
+                               :values [(vpc-id)]}])))
+
+(defn create-ec2-instance-role []
   (iam/create-role
     :role-name ec2-instance-role-name
     :assume-role-policy-document (json/encode
@@ -172,88 +216,6 @@
   (iam/add-role-to-instance-profile
     :instance-profile-name ec2-instance-role-name
     :role-name ec2-instance-role-name))
-
-
-(defn create-service-role []
-  (let [role-name ecs-service-role-name]
-    (iam/create-role
-      :role-name role-name
-      :assume-role-policy-document (json/encode
-                                     {"Version"   "2008-10-17",
-                                      "Statement" [{"Sid"       ""
-                                                    "Effect"    "Allow",
-                                                    "Principal" {"Service" "ecs.amazonaws.com"},
-                                                    "Action"    "sts:AssumeRole"}]}))
-    (iam/attach-role-policy
-      :role-name role-name
-      :policy-arn "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole")))
-
-;; # IP and DNS
-
-(defn create-eip []
-  (ec2/allocate-address
-    :domain DomainType/Vpc))
-
-(defn dns-zone-id []
-  (let [zone-name (str dns-zone-name ".")]
-    (sp/select-one!
-      [:hosted-zones sp/ALL #(= zone-name (:name %)) :id]
-      (r53/list-hosted-zones))))
-
-(defn create-dns-record [action ip]
-  (r53/change-resource-record-sets
-    :hosted-zone-id (dns-zone-id)
-    :change-batch {:changes [{:action              ChangeAction/CREATE
-                              :resource-record-set (merge
-                                                     (if ip
-                                                       {:resource-records [{:value ip}]
-                                                        :ttl              300})
-                                                     {:name dns-name
-                                                      :type RRType/A})}]}))
-
-;; # EC2 resources
-
-(defn authorize-world-ingress [group-id port]
-  (ec2/authorize-security-group-ingress
-    :group-id group-id
-    :ip-protocol "tcp"
-    :from-port port
-    :to-port port
-    :cidr-ip "0.0.0.0/0"))
-
-(defn vpc-id []
-  (sp/select-one!
-    [:vpcs sp/ALL :vpc-id]
-    (ec2/describe-vpcs
-      :filters [{:name   "isDefault"
-                 :values [true]}])))
-
-(defn vpc-subnets []
-  (sp/select [:subnets sp/ALL :subnet-id]
-             (ec2/describe-subnets
-               :filters [{:name   "vpc-id"
-                          :values [(vpc-id)]}])))
-
-(defn create-ec2-instance-security-group [vpc-id]
-  (let [sg-name  ec2-instance-security-group-name
-        group-id (:group-id
-                   (ec2/create-security-group
-                     :group-name sg-name
-                     :description (str project-name " HTTP/S")
-                     :vpc-id vpc-id))]
-    (authorize-world-ingress group-id 80)
-    (authorize-world-ingress group-id 443)
-    group-id))
-
-
-(defn ec2-instance-security-group-id []
-  (sp/select-one! [:security-groups sp/ALL :group-id]
-                  (ec2/describe-security-groups
-                    :filters [{:name   "group-name"
-                               :values [ec2-instance-security-group-name]}
-                              {:name   "vpc-id"
-                               :values [(vpc-id)]}])))
-
 
 (defn env-var-to-ecs-config [var value]
   (str
@@ -285,20 +247,24 @@
                                    :image-ids [image-id])))
       :user-data (encode-base64
                    (str/join "\n"
-                             (concat
-                               ["#!/bin/bash"
-                                "exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1"
-                                (env-var-to-ecs-config "ECS_CLUSTER" ecs-cluster-name)
-                                (env-var-to-ecs-config "ECS_ENABLE_TASK_IAM_ROLE" "true")]
-                               (if allocation-id
-                                 ["yum install -y aws-cli"
-                                  "INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
-                                  (str
-                                    "aws ec2 associate-address --region eu-west-1 --instance-id ${INSTANCE_ID} --allocation-id "
-                                    allocation-id
-                                    " --allow-reassociation")])
-                               [(str "echo \"" (load-ssh-public-key) "\" >> /home/ec2-user/.ssh/authorized_keys")
-                                ""]))))))
+                             ["#!/bin/bash"
+                              "exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1"
+                              (env-var-to-ecs-config "ECS_CLUSTER" ecs-cluster-name)
+                              (env-var-to-ecs-config "ECS_ENABLE_TASK_IAM_ROLE" "true")
+                              "yum install -y aws-cli"
+                              "INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)"
+                              (str
+                                "aws ec2 associate-address --region eu-central-1 --instance-id ${INSTANCE_ID} --allocation-id "
+                                allocation-id
+                                " --allow-reassociation")
+                              (str "echo \"" (load-ssh-public-key) "\" >> /home/ec2-user/.ssh/authorized_keys")
+                              ""])))))
+
+(defn vpc-subnets []
+  (sp/select [:subnets sp/ALL :subnet-id]
+             (ec2/describe-subnets
+               :filters [{:name   "vpc-id"
+                          :values [(vpc-id)]}])))
 
 (defn create-auto-scaling-group []
   (asc/create-auto-scaling-group
@@ -318,10 +284,20 @@
                            ","
                            (vpc-subnets))))
 
+;; # Cloudwatch log group
+
+(defn create-cloudwatch-log-group []
+  (cwl/create-log-group
+    :log-group-name cloudwatch-log-group-name))
+
+(defn cloudwatch-log-group-arn []
+  (sp/select-one [:log-groups sp/ALL :arn]
+                 (cwl/describe-log-groups
+                   :log-group-name-prefix cloudwatch-log-group-name)))
 
 ;; # ECS Service and tasks
 
-(defn create-task-role [env]
+(defn create-task-role []
   (iam/create-role
     :role-name task-role-name
     :assume-role-policy-document (json/encode
@@ -339,10 +315,17 @@
                                       "Action"   ["cloudwatch:PutMetricData"],
                                       "Resource" ["*"]}]})))
 
+(defn role-arn [role-name]
+  (try
+    (sp/select-one! [:role :arn]
+                    (iam/get-role
+                      :role-name role-name))
+    (catch NoSuchEntityException _)))
+
 (defn task-role-arn []
   (role-arn task-role-name))
 
-(defn register-backend-task-definition [backend-version]
+(defn register-backend-task-definition [version]
   (:task-definition
     (ecs/register-task-definition
       :network-mode "host"
@@ -353,7 +336,7 @@
       :container-definitions
       [{:cpu                1024
         :essential          true
-        :image              (str (docker-repository-uri) ":" backend-version)
+        :image              (str (docker-repository-uri) ":" version)
         :log-configuration  {:log-driver "awslogs"
                              :options    {"awslogs-group"  cloudwatch-log-group-name
                                           "awslogs-region" "eu-central-1"}}
@@ -390,7 +373,7 @@
                          :family-prefix task-description-family-name
                          :sort "DESC")))))
 
-(defn create-backend-service [version]
+(defn create-service [version]
   (ecs/create-service
     :service-name service-name
     :cluster ecs-cluster-name
@@ -402,6 +385,23 @@
     :task-definition (find-task-definition version)))
 
 ;; ### deployment
+
+(defn authorize-deployments-by-circleci-user []
+  (iam/put-user-policy
+    :user-name circleci-user-name
+    :policy-name "circle-deploy"
+    :policy-document (json/encode {"Version"   "2012-10-17"
+                                   "Statement" [{"Effect"   "Allow",
+                                                 "Action"   ["ecs:DescribeTaskDefinition",
+                                                             "ecs:ListTaskDefinitions",
+                                                             "ecs:RegisterTaskDefinition",
+                                                             "ecs:DescribeServices",
+                                                             "ecs:ListServices",
+                                                             "ecs:UpdateService"],
+                                                 "Resource" "*"}
+                                                {"Effect"   "Allow",
+                                                 "Action"   ["iam:PassRole"],
+                                                 "Resource" [(task-role-arn)]}]})))
 
 (defn latest-task-definition-arn []
   (sp/select-one! [:task-definition-arns sp/FIRST]
@@ -456,3 +456,5 @@
     :cluster ecs-cluster-name
     :service service-name
     :desired-count 0))
+
+
