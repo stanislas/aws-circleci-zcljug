3e3762198d2700fecfd4dad4e310c078b6795af9
Stanislas Nanchen <stan@hood.ch>
2017-10-19 16:04:22 +0200

    fix app.sh

--- circleci/build/app.sh
+++ circleci/build/app.sh
@@ -2,5 +2,5 @@
 
 JAVA_OPTS="${JAVA_OPTS} -server"
 
-java ${JAVA_OPTS} -cp /app/aws_circleci_zcljug.jar aws_circleci_zcljug.main
+java ${JAVA_OPTS} -cp /app/aws-circleci-zcljug.jar aws_circleci_zcljug.main
 
